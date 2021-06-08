d84430c26dcdcfdacd217d33b9a0bb37ebdeb9d8
Tan <tan10tvo@gmail.com>
2021-06-08 13:32:13 -0600

    commit tree f94bb9151ad8fd0bf26c9463ff1b9b73351830fa

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+customerName = 'bob'
+
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase();
+
+} 
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'the bob'
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'not bob';
+}
+
